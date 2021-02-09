360bdfa8dad21f12e8488151deefc250ae8abc5d
Aneesh Puttur <aputtur@localhost.localdomain>
2021-02-09 12:28:47 -0500

    variable rate

--- cmd/cnf/main.go
+++ cmd/cnf/main.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"github.com/aneeshkp/cloudevents-amqp/pkg/chain"
 	"github.com/aneeshkp/cloudevents-amqp/pkg/types"
+	"log"
 	"math/rand"
 	"net"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -23,6 +25,7 @@ var (
 	udpPort                   = 10001
 	totalMsgCount       int64 = 0
 	totalPerSecMsgCount       = 0
+	rollInMs                  = 1
 )
 
 // init sets initial values for variables used in the function.
@@ -32,36 +35,47 @@ func init() {
 func main() {
 	//var err error
 	states := intiState()
+	var err error
+	envRollInMs := os.Getenv("ROLL_DICE")
+	if envRollInMs != "" {
+		rollInMs, err = strconv.Atoi(envRollInMs)
+		if err != nil {
+			log.Fatal("error reading Roll Time")
+		}
+	}
 
 	transition := [][]float32{
 		{
-			0.5, 0.2, 0.2, 0.0, 0.1,
+			0.8, 0.1, 0.1, 0.0, 0.0,
 		},
 		{
-			0.2, 0.6, 0.0, 0.1, 0.1,
+			0.1, 0.9, 0.1, 0.0, 0.0,
 		},
 		{
-			0.0, 0.3, 0.6, 0.1, 0.0,
+			0.3, 0.3, 0.2, 0.0, 0.0,
 		},
 		{
-			0.1, 0.0, 0.2, 0.5, 0.2,
+			0.2, 0.3, 0.5, 0.0, 0.0,
 		},
 		{
-			0.2, 0.3, 0.0, 0.0, 0.5,
+			0.3, 0.4, 0.3, 0.0, 0.0,
 		},
 	}
 
-	fmt.Printf("Sleeping %d sec...\n", 10)
-	time.Sleep(time.Duration(10) * time.Second)
-
-	diceTicker := time.NewTicker(time.Duration(100) * time.Millisecond)
+	//fmt.Printf("Sleeping %d sec...\n", 10)
+	//time.Sleep(time.Duration(10) * time.Second)
+	diceTicker := time.NewTicker(time.Duration(1) * time.Millisecond)
 	avgPerSecTicker := time.NewTicker(time.Duration(5) * time.Second)
 
+
 	// initialize current state
-	currentStateID := 2
+	currentStateID := 1
 	c := chain.Create(transition, states)
 	currentStateChoice := c.GetStateChoice(currentStateID)
 	currentStateID = currentStateChoice.Item.(int)
+	//rand.Seed(time.Now().UnixNano())
+	min := 1
+	max := 4
 
 	for { //nolint:gosimple
 		select {
@@ -69,15 +83,30 @@ func main() {
 			currentStateChoice := c.GetStateChoice(currentStateID)
 			currentStateID = currentStateChoice.Item.(int)
 			currentState := c.GetState(currentStateID)
-			r := rand.Intn(100)
+			//	r := rand.Intn(50)
+			if currentState.Payload.Probability > 0 {
+				//for i := 1; i <= currentState.Payload.ID; i++ {
+				_ = Event(currentState.Payload)
+				totalMsgCount++
+				totalPerSecMsgCount++
+				//}
+			}
+			//diceTicker.Stop()
+			//newTime:=rand.Intn(max - min + 1) + min
+			//diceTicker = time.NewTicker(time.Duration(newTime) * time.Millisecond)
+			/*r := rand.Intn(100)
 			if currentState.Payload.Probability >= r {
 				_ = Event(currentState.Payload)
 				totalMsgCount++
 				totalPerSecMsgCount++
-			}
+			}*/
 		case <-avgPerSecTicker.C:
-			fmt.Printf("Total message sent last 5 secs %d  mps: %2.2f\n", totalPerSecMsgCount, float64(totalPerSecMsgCount)/5)
+			fmt.Printf("Total message sent mps: %2.2f\n", float64(totalPerSecMsgCount)/1)
 			totalPerSecMsgCount = 0
+			diceTicker.Stop()
+			newTime:=rand.Intn(max - min + 1) + min
+			diceTicker = time.NewTicker(time.Duration(newTime) * time.Millisecond)
+
 		}
 	}
 
@@ -91,7 +120,7 @@ func intiState() []chain.State {
 	states := []chain.State{
 		{
 			Payload: types.Message{
-				ID:      1,
+				ID:      25,
 				StateID: 1,
 				Source: fmt.Sprintf("Node: %s Pod: %s NameSpace: %s IP:%s",
 					os.Getenv("MY_NODE_NAME"),
@@ -106,7 +135,7 @@ func intiState() []chain.State {
 		},
 		{
 			Payload: types.Message{
-				ID:      2,
+				ID:      10,
 				StateID: 2,
 				Source: fmt.Sprintf("Node: %s Pod: %s NameSpace: %s IP:%s",
 					os.Getenv("MY_NODE_NAME"),
@@ -115,14 +144,14 @@ func intiState() []chain.State {
 					os.Getenv("MY_POD_IP"),
 				),
 				Msg:         fmt.Sprintf("Event Occurred %s:", "Internet Down"),
-				Probability: 75,
+				Probability: 99,
 			},
 			ID: 2,
 		},
 		{
 			Payload: types.Message{
-				StateID: 3,
-				ID:      3,
+				StateID: 1,
+				ID:      50,
 				Source: fmt.Sprintf("Node: %s Pod: %s NameSpace: %s IP:%s",
 					os.Getenv("MY_NODE_NAME"),
 					os.Getenv("MY_POD_NAME"),
@@ -130,25 +159,37 @@ func intiState() []chain.State {
 					os.Getenv("MY_POD_IP"),
 				),
 				Msg:         fmt.Sprintf("Event Occurred %s:", "Amazon package delivered"),
-				Probability: 50,
+				Probability: 99,
 			},
 			ID: 3,
 		},
 		{
 			Payload: types.Message{
-				ID:          4,
-				StateID:     4,
-				Source:      "",
-				Probability: 0,
+				ID:      4,
+				StateID: 4,
+				Source: fmt.Sprintf("Node: %s Pod: %s NameSpace: %s IP:%s",
+					os.Getenv("MY_NODE_NAME"),
+					os.Getenv("MY_POD_NAME"),
+					os.Getenv("MY_POD_NAMESPACE"),
+					os.Getenv("MY_POD_IP"),
+				),
+				Msg:         fmt.Sprintf("Event Occurred %s:", "Car breakdown"),
+				Probability: 99,
 			},
 			ID: 4,
 		},
 		{
 			Payload: types.Message{
-				ID:          5,
-				StateID:     5,
-				Source:      "",
-				Probability: 0,
+				ID:      5,
+				StateID: 5,
+				Source: fmt.Sprintf("Node: %s Pod: %s NameSpace: %s IP:%s",
+					os.Getenv("MY_NODE_NAME"),
+					os.Getenv("MY_POD_NAME"),
+					os.Getenv("MY_POD_NAMESPACE"),
+					os.Getenv("MY_POD_IP"),
+				),
+				Msg:         fmt.Sprintf("Event Occurred %s:", "Temp too high"),
+				Probability: 99,
 			},
 			ID: 5,
 		},
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/google/uuid v1.1.1
 	github.com/jmcvetta/randutil v0.0.0-20150817122601-2bb1b664bcff
 	github.com/stretchr/testify v1.5.1
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/yaml.v2 v2.3.0
 )
 
